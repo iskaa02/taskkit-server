b31b70411eb94723e43d983f95c0acaeca30aefb
iskaa02 <eaemk@outlook.sa>
2022-06-17 02:21:36 +0200

    add pull changes end point

---
+++ api/sync/getChanges.go
@@ -0,0 +1,88 @@
+package sync
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/iskaa02/taskkit-server/db/models"
+)
+
+func getChanges(lastPulled time.Time, q *models.Queries) changes {
+	return changes{
+		List: getListChanges(lastPulled, q),
+		Task: getTaskChanges(lastPulled, q),
+	}
+}
+
+func getTaskChanges(lastPulled time.Time, q *models.Queries) taskChanges {
+	ctx := context.Background()
+	rawCreated, err := q.GetNewlyCreatedTasks(ctx, lastPulled)
+	if err != nil {
+		fmt.Println(err)
+	}
+	created := make([]rawTask, len(rawCreated))
+	for i := range rawCreated {
+		created[i] = rawTask(rawCreated[i])
+	}
+
+	rawUpdated, err := q.GetNewlyUpdatedTasks(ctx, lastPulled)
+	if err != nil {
+		fmt.Println(err)
+	}
+	updated := make([]rawTask, len(rawUpdated))
+	for i := range rawUpdated {
+		created[i] = rawTask(rawCreated[i])
+	}
+
+	deleted, err := q.GetNewlyDeletedTasks(ctx, lastPulled)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return taskChanges{
+		Created: created,
+		Updated: updated,
+		Deleted: deleted,
+	}
+}
+
+func getListChanges(lastPulled time.Time, q *models.Queries) listChanges {
+	ctx := context.Background()
+	rawCreated, err := q.GetNewlyCreatedLists(ctx, lastPulled)
+	if err != nil {
+		fmt.Println(err)
+	}
+	created := make([]rawList, len(rawCreated))
+	for i := range rawCreated {
+		created[i] = toRawList(rawCreated[i])
+	}
+	rawUpdated, err := q.GetNewlyUpdatedLists(ctx, lastPulled)
+	if err != nil {
+		fmt.Println(err)
+	}
+	updated := make([]rawList, len(rawUpdated))
+	for i := range rawUpdated {
+		created[i] = toRawList(rawCreated[i])
+	}
+
+	deleted, err := q.GetNewlyDeletedLists(ctx, lastPulled)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return listChanges{
+		Created: created,
+		Updated: updated,
+		Deleted: deleted,
+	}
+}
+
+func toRawList(l models.GetNewlyCreatedListsRow) rawList {
+	return rawList{
+		Name: l.Name,
+		ID:   l.ID,
+		Theme: theme{
+			Primary:   l.Primary,
+			Secondary: l.Secondary,
+		},
+	}
+}
--- api/sync/pullChanges.go
+++ api/sync/pullChanges.go
@@ -1,13 +1,39 @@
 package sync
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/iskaa02/taskkit-server/db/models"
+	"gopkg.in/guregu/null.v4"
 )
 
 type changes struct {
 	List listChanges `json:"lists"`
 	Task taskChanges `json:"tasks"`
 }
+type pullChangesReq struct {
+	LastPulledAt null.Int `json:"lastPulledAt"`
+}
 
 func (s sync) PullChanges(w http.ResponseWriter, r *http.Request) {
+	args := pullChangesReq{}
+	err := json.NewDecoder(r.Body).Decode(&args)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "bad JSON format", http.StatusBadRequest)
+		return
+	}
+	q := models.New(s.db)
+	unixTS := int64(0)
+	if args.LastPulledAt.Valid {
+		unixTS = args.LastPulledAt.Int64
+	}
+	lastPulledAt := time.Unix(unixTS, 0)
+	fmt.Println(lastPulledAt.Year())
+	changes := getChanges(lastPulledAt, q)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(changes)
 }
